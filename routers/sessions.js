0b5360740bcfa09a58aef7aa66a327f6054c0c42
Duy Nguyen <duynguyenquykhanh@gmail.com>
2022-05-22 14:07:36 -0700

    update backend

--- routers/sessions.js
+++ routers/sessions.js
@@ -133,7 +133,7 @@ router.post("/upload-video", upload.single("my-video"), async (req, res) => {
           `ffmpeg -i ${newVideoPath} -ss ${start_trim} -to ${end_trim} -c:v copy -ac 1 ${trimAudioPath}`
         );
         await exec(
-          `ffmpeg -i ${newVideoPath} -ss ${start_trim} -to ${end_trim} -c:v copy -ac 1 ${trimVideoPath}`
+          `ffmpeg -i ${newVideoPath} -ss ${start_trim} -to ${end_trim} -c:v copy ${trimVideoPath}`
         );
 
         await gcpStorage.bucket(bucketURL).upload(trimAudioPath, {
