6af14fb6acf60d7c1317d470c4850fa2e2d96d78
Duy Nguyen <duynguyenquykhanh@gmail.com>
2022-06-14 17:41:55 -0700

    update socket

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const bodyParse = require("body-parser");
-const users = require("./routers/users");
+const usersRoute = require("./routers/users");
+const http = require("http");
+const server = http.createServer(app);
 
-//MiddleWare
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+//CORS
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"),
     res.header(
@@ -20,17 +28,69 @@ app.use((req, res, next) => {
   next();
 });
 
-//TEST
+//Middlewares
 app.use(cors());
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
 app.use(morgan("dev"));
 
-app.use("/users", users);
+// Routes
+app.use("/users", usersRoute);
 
-// HELLo
+// Default
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "Hello" });
 });
+
+// Room Calling
+const users = {};
+const socketToRoom = {};
+
+io.on("connection", (socket) => {
+  socket.on("join room", (roomID) => {
+    if (users[roomID]) {
+      const length = users[roomID].length;
+      if (length === 4) {
+        socket.emit("room full");
+        return;
+      }
+      users[roomID].push(socket.id);
+    } else {
+      users[roomID] = [socket.id];
+    }
+    socketToRoom[socket.id] = roomID;
+    const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+    socket.emit("all users", usersInThisRoom);
+  });
+
+  socket.on("sending signal", (payload) => {
+    io.to(payload.userToSignal).emit("user joined", {
+      signal: payload.signal,
+      callerID: payload.callerID,
+    });
+  });
+
+  socket.on("returning signal", (payload) => {
+    io.to(payload.callerID).emit("receiving returned signal", {
+      signal: payload.signal,
+      id: socket.id,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((id) => id !== socket.id);
+      users[roomID] = room;
+    }
+    socket.broadcast.emit("user left", socket.id);
+  });
+
+  socket.on("change", (payload) => {
+    socket.broadcast.emit("change", payload);
+  });
+});
+
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("Running Server at " + port));
+server.listen(port, () => console.log("Running Server at " + port));
